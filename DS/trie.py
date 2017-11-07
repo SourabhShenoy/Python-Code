02ddd4d39039701798bbd447d1b71be47e2df5e9
SourabhShenoy <sourabhsshenoy@gmail.com>
2017-11-07 02:37:32 -0600

    Removed unwanted files

--- DS/trie.py
+++
@@ -1,9 +0,0 @@
-#https://nickstanisha.github.io/2015/11/21/a-good-trie-implementation-in-python.html
-class Node:
-	def __init__(self,label=None,data=None):
-		self.data = data
-		self.label = label
-		self.children = dict()
-	
-	def addChild(self,key,data=None):
-		
--- Random/to-do/random_python_stuff.py
+++
@@ -1,1 +0,0 @@
-http://codeblog.dhananjaynene.com/2011/06/10-python-one-liners-to-impress-your-friends/
